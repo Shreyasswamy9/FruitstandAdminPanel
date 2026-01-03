7f3fb8865fa2f7560c1f782b3ce69acb279c4998
Shreyas Swamy <ss263@fordham.edu>
2026-01-03 16:17:20 +0530

    login page

--- admin-server/api/index.ts
+++ admin-server/api/index.ts
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import { createAdminApp } from "../src/adminApp";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 const app = createAdminApp();
 
-export default function handler(req: any, res: any): void {
+export default function handler(req: VercelRequest, res: VercelResponse): void {
   app(req, res);
 }
